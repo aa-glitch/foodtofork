f985df1d8a5796a028536fdcad83ad64ad2cc681
Anshul Anand <ansh.anand@gmail.com>
2019-12-23 21:17:30 -0500

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+import Search from './models/Search'
+import { elements }  from './views/base'
+import * as searchView from './views/searchView'
+
+/** Global state 
+ * - search object
+ * - current recipie object
+ * - shopping list object
+ * - liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    //1) get a query from the view
+    const query = searchView.getInput();
+    console.log(query);
+
+    if (query) {
+        // 2) New search object and add it to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        //console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e=> {
+    e.preventDefault();
+    controlSearch();
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            console.log(`query parameter is ${this.query}`);
+            const response = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = response.data.recipes;
+        } catch (error) {
+            alert(error);
+        }
+        
+    }
+}
